f0b59f2b441679993132189721fe76d022d17f23
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-07-06 09:27:55 +0900

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const App = () => <div />;
+const ENDPOINT = "https://ccc-treasure-api.an.r.appspot.com";
+
+const App = () => {
+  const [data, setData] = useState<{
+    treasure: string;
+  }>();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${ENDPOINT}`);
+      const json = await res.json();
+      setData(json);
+    })();
+  }, []);
+
+  return <p>{data?.treasure}</p>;
+};
 
 export default App;
